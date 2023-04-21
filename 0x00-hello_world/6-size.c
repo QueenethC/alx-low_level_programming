c19877bb0747d3cccc7d597f0881673e08819f8b
QueenethC <queenethchinda4@gmail.com>
2023-04-20 18:14:53 -0700

    Hello World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf(Size of a char: %d byte(s)\n", size of (char));
+    printf(Size of an init: %d byte(s)\n", size of (init));
+    printf(Size of a long init: %d byte(s)\n", size of (long init));
+    printf(Size of a long long init: %d byte(s)\n", size of (long long init));
+    printf(Size of a float: %d byte(s)\n", size of (float));
+    return (0);
+}
+
