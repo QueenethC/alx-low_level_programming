9a9db68b6ee4374db0c0de10cbb315619af60ae6
QueenethC <queenethchinda4@gmail.com>
2023-06-05 13:58:43 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns The nmber of Elements in a linked list
+ * @h: linked list of type listint_t to Traverse
+ *
+ * Return: Number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
